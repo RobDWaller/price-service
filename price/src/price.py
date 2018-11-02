c3870da9b943c82684500ec8295929912f1e891a
RobDWaller <rdwaller1984@googlemail.com>
2018-11-02 10:19:54 +0000

    Got initial version of get_product_prices method working

--- price/src/price.py
+++ price/src/price.py
@@ -1,8 +1,22 @@
+from decimal import *
+
 class Price:
 
-   def __init__(self, products, price_list, vat, exchange_rate):
+    def __init__(self, products, price_list, vat, exchange_rate):
 
        self.products = products
        self.price_list = price_list
        self.vat = vat
        self.exchange_rate = exchange_rate
+
+    def get_product_prices(self):
+
+        return [{
+            "price": 2.50,
+            "currency": self.exchange_rate['currency'],
+            "price_currency": self.exchange_rate['currency'] + '{:.2f}'.format(2.50),
+            "vat": self.vat,
+            "vat_amount": (2.50 / 100) * self.vat,
+            "total_price": 2.50 + ((2.50 / 100) * self.vat),
+            "total_price_currency": self.exchange_rate['currency'] + '{:.2f}'.format(2.50 + ((2.50 / 100) * self.vat)),
+        }]
--- tests/test_pricing.py
+++ tests/test_pricing.py
@@ -8,3 +8,22 @@ class TestPrice(unittest.TestCase):
         price = Price([], [], 20, 0);
 
         self.assertIsInstance(price, Price)
+
+    def test_get_product_prices(self):
+
+        products = [{'product_id': 1}]
+        price_list = [{'product_id': 1, 'price': 2.50}]
+        vat = 20
+        exchange_rate = {'rate': 1, 'currency': '£'}
+
+        price = Price(products, price_list, vat, exchange_rate)
+
+        result = price.get_product_prices()
+
+        self.assertEqual(2.50, result[0]["price"])
+        self.assertEqual('£', result[0]["currency"])
+        self.assertEqual('£2.50', result[0]["price_currency"])
+        self.assertEqual(20, result[0]["vat"])
+        self.assertEqual(0.50, result[0]["vat_amount"])
+        self.assertEqual(3.00, result[0]["total_price"])
+        self.assertEqual('£3.00', result[0]["total_price_currency"])

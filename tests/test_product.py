73f85c0d6ba555dba9d0b9aef397895f86c4b025
RobDWaller <rdwaller1984@googlemail.com>
2018-11-03 15:37:05 +0000

    Added a secondary get_products test to confirm working

--- tests/test_product.py
+++ tests/test_product.py
@@ -19,3 +19,22 @@ class TestProduct(unittest.TestCase):
         product = Product(products)
 
         self.assertEqual(len(product.get_products()), 5)
+
+    def test_products_two(self):
+
+        products = [
+            {'product_id': 1, 'quantity': 2},
+            {'product_id': 2, 'quantity': 3},
+            {'product_id': 3, 'quantity': 1}
+        ]
+
+        product = Product(products)
+        result = product.get_products()
+
+        self.assertEqual(len(result), 6)
+        self.assertEqual(result[0]['product_id'], 1)
+        self.assertEqual(result[1]['product_id'], 1)
+        self.assertEqual(result[2]['product_id'], 2)
+        self.assertEqual(result[3]['product_id'], 2)
+        self.assertEqual(result[4]['product_id'], 2)
+        self.assertEqual(result[5]['product_id'], 3)
